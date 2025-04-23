5902974428f17250f8e533e03ff4e477a6b3a2ee
DanielOv1103 <danieloliveiav1103@gmail.com>
2025-04-22 20:22:00 -0500

    first commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <nav className="bg-white p-4 shadow mb-6">
         <Link to="/" className="mr-4 text-blue-600 hover:underline">Home</Link>
-        <Link to="/create" className="text-blue-600 hover:underline">Crear Evento</Link>
+        <Link to="/create" className="text-blue-600 hover:underline">Crear Eventos</Link>
       </nav>
       <div className="px-4">
         <Routes>
