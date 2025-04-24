1d64d025a2e55172a46e063080994b4a73d1eae6
DanielOv1103 <danieloliveirav1103@gmail.com>
2025-04-24 15:02:34 -0500

    Fix: ui details

--- frontend/src/app/Home.jsx
+++ frontend/src/app/Home.jsx
@@ -3,7 +3,6 @@ import { NavBar } from "../modules/index"
 export default function Home() {
     return (
         <>
-            <NavBar />
             <div className="App">
                 <h1 className="text-3xl font-bold text-center mb-6 text-black">Lista de Eventos</h1>
             </div>
